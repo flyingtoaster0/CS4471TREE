eec265081d60656de7743a327deb2e18f664273c
Timothy Schonberger <timothy.schonberger@gmail.com>
2013-11-21 19:54:58 -0500

    Added a Clickable class and PushButtons

---
+++ PredatorAndPrey/PredatorAndPrey/Clickable.cpp
@@ -0,0 +1,25 @@
+#include "Clickable.h"
+
+Clickable::Clickable()
+{
+	
+	this->x = 0;
+	this->y = 0;
+	this->width = 0;
+	this->height = 0;
+}
+
+Clickable::Clickable(float x, float y, float height, float width)
+{
+	this->x = x;
+	this->y = y;
+	this->width = width;
+	this->height = height;
+}
+
+bool Clickable::overlap(float x, float y)
+{
+	if((x > this->x) && (x < this->x + this->width) && (y > this->y) && (y < this->y + this->height))
+		return true;
+	return false;
+}
